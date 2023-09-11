512897d715a091a43963db1a0a59e565e8cdf5a6
Gustavo Soares <guusoaresss@icloud.com>
2023-09-11 10:46:11 -0300

    Conteudo Página Nota Fiscal

--- src/routes/NotaFiscal/index.jsx
+++ src/routes/NotaFiscal/index.jsx
@@ -3,7 +3,23 @@ export default function NotaFiscal(){
     return(
         <main>
             <h1>Nota Fiscal</h1>
-            <p>teste</p>
+            <label for="nome">Número da NF:</label><br></br>
+            <input type="text" id="nome" name="nome"></input><br></br>
+            <label for="nome">Empresa emissora:</label><br></br>
+            <input type="text" id="nome" name="nome"></input><br></br>
+            <label for="nome">Data de emissão:</label><br></br>
+            <input type="text" id="nome" name="nome"></input><br></br>
+            <label for="nome">Produto:</label><br></br>
+            <input type="text" id="nome" name="nome"></input><br></br>
+            <label for="nome">Modelo:</label><br></br>
+            <input type="text" id="nome" name="nome"></input><br></br>
+            <label for="nome">Cor:</label><br></br>
+            <input type="text" id="nome" name="nome"></input><br></br>
+            <label for="nome">Valor Pago:</label><br></br>
+            <input type="text" id="nome" name="nome"></input><br></br>
+            <input type="checkbox" id="meuCheckbox" name="meuCheckbox"></input>
+            <label for="meuCheckbox">Concordo com os Termos e Condições</label><br></br>
+            <button>Enviar</button>
         </main>
     )
 }
