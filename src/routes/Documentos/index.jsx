85c77c6c5a6fb2c75852f2b33027d13742f27615
Gustavo Soares <guusoaresss@icloud.com>
2023-09-11 10:52:05 -0300

    Conteudo Documentos

--- src/routes/Documentos/index.jsx
+++ src/routes/Documentos/index.jsx
@@ -3,6 +3,12 @@ export default function Documentos(){
     return(
         <main>
             <h1>Documentos</h1>
+            <p>
+            Agora vamos iniciar a etapa de validação dos documentos.<br/>
+            Para isso verifique a iluminação do local e a nitidez da foto que sera enviada.<br/>
+            Dica: posicione o documento dentro da área quadriculada.
+            </p>
+            <button>Começar</button>
         </main>
     )
 }
