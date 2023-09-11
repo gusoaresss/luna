4cac78b6a45c9cfedb8ac093a463a966d41fdca8
Gustavo Soares <guusoaresss@icloud.com>
2023-09-11 11:24:05 -0300

    Modificações

--- src/routes/Modificações/index.jsx
+++ src/routes/Modificações/index.jsx
@@ -3,6 +3,23 @@ export default function Modificacao(){
     return(
         <main>
             <h1>Modificações</h1>
+            <input type="checkbox" id="meuCheckbox" name="meuCheckbox"></input>
+            <label for="meuCheckbox">Catracra</label><br></br>
+            <input type="checkbox" id="meuCheckbox" name="meuCheckbox"></input>
+            <label for="meuCheckbox">Manopla</label><br></br>
+            <input type="checkbox" id="meuCheckbox" name="meuCheckbox"></input>
+            <label for="meuCheckbox">Marcha</label><br></br>
+            <input type="checkbox" id="meuCheckbox" name="meuCheckbox"></input>
+            <label for="meuCheckbox">Pedal</label><br></br>
+            <input type="checkbox" id="meuCheckbox" name="meuCheckbox"></input>
+            <label for="meuCheckbox">Pneu frontal</label><br></br>
+            <input type="checkbox" id="meuCheckbox" name="meuCheckbox"></input>
+            <label for="meuCheckbox">Pneu traseiro</label><br></br>
+            <input type="checkbox" id="meuCheckbox" name="meuCheckbox"></input>
+            <label for="meuCheckbox">Banco</label><br></br>
+            <input type="checkbox" id="meuCheckbox" name="meuCheckbox"></input>
+            <label for="meuCheckbox">Guidão</label><br></br>
+            <button>Continuar</button>
         </main>
     )
 }
