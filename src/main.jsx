8e86233c583fd0aef157728d69a2b76593835d72
Gustavo Soares <guusoaresss@icloud.com>
2023-09-11 09:52:51 -0300

    Fazendo imports no main

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Documentos from './routes/Documentos/index.jsx'
+import Error from './routes/Error/index.jsx'
+import FotoDaBike from './routes/FotoDaBike/index.jsx'
+import FotoPessoal from './routes/FotoPessoal/index.jsx'
+import FotoRG from './routes/FotoRG/index.jsx'
+import FotoRosto from './routes/FotoRosto/index.jsx'
+import Modificacao from './routes/Modificações/index.jsx'
+import NotaFiscal from './routes/NotaFiscal/index.jsx'
+import ParteFrontalBike from './routes/ParteFrontalBike/index.jsx'
+import ParteTraseiraBike from './routes/ParteTraseiraBike/index.jsx'
+import ValidarBike from './routes/ValidarBike/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
