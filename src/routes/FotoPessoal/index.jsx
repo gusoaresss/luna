4fdd4ca57349b754c87508580f6d94d32e2b7cc8
Gustavo Soares <guusoaresss@icloud.com>
2023-09-11 11:06:08 -0300

    Foto Pessoal Conteudo

--- src/routes/FotoPessoal/index.jsx
+++ src/routes/FotoPessoal/index.jsx
@@ -3,6 +3,12 @@ export default function FotoPessoal(){
     return(
         <main>
             <h1>Foto Pessoal</h1>
+            <p>
+            Agora precisamos tirar uma foto sua para validar os dados.<br/>
+            Para isso verifique a iluminação do local e caso esteja usando, retire o óculos.<br/>
+            Dica: posicione o seu rosto dentro da área circular.
+            </p>
+            <button>Começar</button>
         </main>
     )
 }
