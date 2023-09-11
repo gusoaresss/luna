1d48a279a6aedc8b23525c277cfc247d9917654f
Gustavo Soares <guusoaresss@icloud.com>
2023-09-11 11:19:46 -0300

    Conteudo validar Bike + Foto rosto

--- src/routes/FotoRosto/index.jsx
+++ src/routes/FotoRosto/index.jsx
@@ -3,6 +3,9 @@ export default function FotoRosto(){
     return(
         <main>
             <h1>Foto Rosto</h1>
+            <button id="startCamera">Abrir Câmera</button>
+            <p>Tem certeza que a imagem está nítida?</p>
+            <button>Enviar</button>
         </main>
     )
 }
--- src/routes/ValidarBike/index.jsx
+++ src/routes/ValidarBike/index.jsx
@@ -3,6 +3,16 @@ export default function ValidarBike(){
     return(
         <main>
             <h1>Validar Bike</h1>
+            <p>
+            Agora vamos iniciar a etapa de validação da sua bike.<br/>
+            Para isso, informe:<br/>
+            A sua Bike possui modificações?
+            </p>
+            <input type="checkbox" id="meuCheckbox" name="meuCheckbox"></input>
+            <label for="meuCheckbox">Sim, possui modificações</label><br></br>
+            <input type="checkbox" id="meuCheckbox" name="meuCheckbox"></input>
+            <label for="meuCheckbox">Não possui modificação.</label><br></br>
+            <button>Continuar</button>
         </main>
     )
 }
