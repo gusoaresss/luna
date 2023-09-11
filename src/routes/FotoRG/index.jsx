fe4676cd8c92ddcdd848fe88395e1e9879af52fa
Gustavo Soares <guusoaresss@icloud.com>
2023-09-11 11:04:04 -0300

    Foto Rg

--- src/routes/FotoRG/index.jsx
+++ src/routes/FotoRG/index.jsx
@@ -3,6 +3,9 @@ export default function FotoRG(){
     return(
         <main>
             <h1>Foto RG</h1>
+            <button id="startCamera">Abrir Câmera</button>
+            <p>Tem certeza que a imagem está nítida?</p>
+            <button>Enviar</button>
         </main>
     )
 }
