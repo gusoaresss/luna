b5c28d10475b4a6b195bf1ea5ddde26dc1969f7f
Gustavo Soares <guusoaresss@icloud.com>
2023-09-11 11:25:56 -0300

    Parte Frontal Bike

--- src/routes/ParteFrontalBike/index.jsx
+++ src/routes/ParteFrontalBike/index.jsx
@@ -3,6 +3,9 @@ export default function ParteFrontalBike(){
     return(
         <main>
             <h1>Parte Frontal Bike</h1>
+            <button id="startCamera">Abrir Câmera</button>
+            <p>Tem certeza que a imagem está nítida?</p>
+            <button>Enviar</button>
         </main>
     )
 }
