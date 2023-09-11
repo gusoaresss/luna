42aae83db34df09c72d0daddc0c8c78c53ebd3ce
Gustavo Soares <guusoaresss@icloud.com>
2023-09-11 11:25:39 -0300

    Conteudos

--- src/routes/FotoDaBike/index.jsx
+++ src/routes/FotoDaBike/index.jsx
@@ -3,6 +3,9 @@ export default function FotoDaBike(){
     return(
         <main>
             <h1>Foto da Bike</h1>
+            <button id="startCamera">Abrir Câmera</button>
+            <p>Tem certeza que a imagem está nítida?</p>
+            <button>Enviar</button>
         </main>
     )
 }
--- src/routes/ParteTraseiraBike/index.jsx
+++ src/routes/ParteTraseiraBike/index.jsx
@@ -3,6 +3,9 @@ export default function ParteTraseiraBike(){
     return(
         <main>
             <h1>Parte Traseira Bike</h1>
+            <button id="startCamera">Abrir Câmera</button>
+            <p>Tem certeza que a imagem está nítida?</p>
+            <button>Enviar</button>
         </main>
     )
 }
