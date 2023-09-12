44c929aef988c9fe111017bebf723749e50b7bb4
Victor <Virtu_gatu5467@hotmail.com>
2023-09-12 13:14:05 -0300

    integrante adicionado

--- src/routes/Integrantes/index.jsx
+++ src/routes/Integrantes/index.jsx
@@ -6,6 +6,7 @@ export default function Integrantes(){
             <p>RM:97850 - Gustavo Soares Fosaluza</p>
             <p>RM:551247 - Murilo Caumo Murgia</p>
             <p>RM:99577 - Guilherme Dias Gomes</p>
+            <p>RM:99661 - Victor Augusto Cifontes de Oliveira</p>
         </main>
     )
 }
