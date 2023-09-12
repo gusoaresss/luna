7f636dfb18159140da1b015bc82001f377d24381
Guilherme Dias Gomes <guilhermediasgomes2@gmail.com>
2023-09-12 13:32:13 -0300

    alterações finais

--- src/routes/Integrantes/index.jsx
+++ src/routes/Integrantes/index.jsx
@@ -3,10 +3,11 @@ export default function Integrantes(){
     return(
         <main>
             <h1>Integrantes</h1>
-            <p>RM:97850 - Gustavo Soares Fosaluza</p>
+            <p>RM:97850 - Gustavo Soares Fosaluza - 1TDSPL</p>
             <p>RM:551247 - Murilo Caumo Murgia</p>
-            <p>RM:99577 - Guilherme Dias Gomes</p>
+            <p>RM:99577 - Guilherme Dias Gomes - 1TDSR</p>
             <p>RM:99661 - Victor Augusto Cifontes de Oliveira</p>
+            <h1><a href="https://github.com/gusoaresss/luna" className="repositorio">| Acesse o nosso repositório |</a></h1>
         </main>
     )
 }
