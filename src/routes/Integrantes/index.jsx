3aff8cc60e1315e1472e620b28db21ff8b684b23
Guilherme Dias Gomes <guilhermediasgomes2@gmail.com>
2023-09-11 22:47:57 -0300

    Adicionando integrante

--- src/routes/Integrantes/index.jsx
+++ src/routes/Integrantes/index.jsx
@@ -5,6 +5,7 @@ export default function Integrantes(){
             <h1>Integrantes</h1>
             <p>RM:97850 - Gustavo Soares Fosaluza</p>
             <p>RM:551247 - Murilo Caumo Murgia</p>
+            <p>RM:99577 - Guilherme Dias Gomes</p>
         </main>
     )
 }
