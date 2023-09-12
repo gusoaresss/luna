35eb66a038f95429d67db4fdf79483c9b3b36d4b
Gustavo Soares <guusoaresss@icloud.com>
2023-09-11 22:00:46 -0300

    Meu RM

--- src/routes/Integrantes/index.jsx
+++ src/routes/Integrantes/index.jsx
@@ -3,6 +3,7 @@ export default function Integrantes(){
     return(
         <main>
             <h1>Integrantes</h1>
+            <p>RM:97850 - Gustavo Soares Fosaluza</p>
         </main>
     )
 }
