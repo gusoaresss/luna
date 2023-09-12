03f165bdf23870c0b0dedfa32ae8ff25b1af8523
Gustavo Soares <guusoaresss@icloud.com>
2023-09-11 21:13:09 -0300

    Cabeçalho

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
 import Rodape from './components/Rodape'
+import Cabecalho from './components/Cabecalho'
 
 function App(){
 
   return(
     <>
+      <Cabecalho/>
       <Outlet/>
       <Rodape/>
     </>
